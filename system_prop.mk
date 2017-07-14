d7fb5c6f103a54dfa96b4779e5f7217e7d476fe1
Jerry Zhang <zhangjerry@google.com>
2017-07-14 11:36:32 -0700

    Set sys.usb.ffs.aio_compat
    The kernel does not support aio with ffs.
    Set the property so adb/mtp know to use the compatible
    functions.
    
    Bug: 37916658
    Test: Use adb / use adb in recovery
    Change-Id: Id35a05fcb9952dcf7239bd15a61cc620537419bc

--- system_prop.mk
+++ system_prop.mk
@@ -229,7 +229,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.adb.secure=0 \
     sys.usb.controller=a800000.dwc3 \
     sys.usb.rndis.func.name=gsi \
-    sys.usb.rmnet.func.name=gsi
+    sys.usb.rmnet.func.name=gsi \
+    sys.usb.ffs.aio_compat=1
 
 # Vendor
 PRODUCT_PROPERTY_OVERRIDES += \
