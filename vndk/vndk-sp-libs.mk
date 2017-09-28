ed377a2e19751f08a4687f08a3968f5de42c8648
Justin Yun <justinyun@google.com>
2017-09-28 13:22:01 +0900

    Remove all copies of vndk-sp modules
    With BOARD_VNDK_VERSION=current, the vendor variant of vndk-sp will
    be installed to system/lib(64)/vndk-sp by build system if they have
    "vndk: { enabled: true, support_system_process: true }" in their
    Android.bp files.
    
    Skip generating the copies of vndk-sp modules when BOARD_VNDK_VERSION
    is defined.
    
    Bug: 65853349
    Test: build and boot with marlin/sailfish
    Change-Id: I49039aa876b8559383a40701f6c04791162a96dc

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -1,34 +1,32 @@
-VNDK_SP_LIBRARIES := \
-    android.hidl.memory@1.0 \
-    libRSCpuRef \
-    libRSDriver \
-    libRS_internal \
-    libbcinfo \
-    libblas \
-    libcompiler_rt \
-    libft2 \
-    libhidlmemory \
-    libion \
-    libpng \
-
 ifndef BOARD_VNDK_VERSION
 
-VNDK_SP_LIBRARIES += \
+VNDK_SP_LIBRARIES := \
     android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.mapper@2.0 \
     android.hardware.graphics.common@1.0 \
+    android.hidl.memory@1.0 \
     android.hardware.renderscript@1.0 \
     libhwbinder \
     libbase \
     libcutils \
     libhardware \
+    libhidlbase \
+    libhidltransport \
     libutils \
     libc++ \
+    libRS_internal \
+    libRSDriver \
+    libRSCpuRef \
+    libbcinfo \
+    libblas \
+    libft2 \
+    libhidlmemory \
+    libion \
+    libpng \
+    libcompiler_rt \
     libbacktrace \
     libunwind \
     libunwindstack \
     liblzma \
-    libhidlbase \
-    libhidltransport \
 
 endif
